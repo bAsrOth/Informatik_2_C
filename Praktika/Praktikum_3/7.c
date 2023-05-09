40d5f39533fae61de4ffec5e2c2d92a0f6bf413a
bAsrOth <[basroth@protonmail.com]>
2023-05-09 08:46:33 +0200

    7.c added

---
+++ Praktika/Praktikum_3/7.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    int zahl;
+    printf("Bitte geben sie eine Zahl (0..255) ein: ");
+    scanf("%d",&zahl);
+    
+    int bit1 = zahl % 2;
+    zahl = zahl /2;
+    int bit2 = zahl % 2;
+    zahl = zahl /2;
+    int bit3 = zahl % 2;
+    zahl = zahl /2;
+    int bit4 = zahl % 2;
+    zahl = zahl /2;
+    int bit5 = zahl % 2;
+    zahl = zahl /2;
+    int bit6 = zahl % 2;
+    zahl = zahl /2;
+    int bit7 = zahl % 2;
+    zahl = zahl /2;
+    int bit8 = zahl % 2;
+
+    printf("%i%i%i%i %i%i%i%i\n",bit1,bit2,bit3,bit4,bit5,bit6,bit7,bit8);
+    printf("%8b",zahl);
+
+    getch();
+    // bit = zahl/2 % 2;
+    // zahl = zahl /2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+
+    // bit = zahl%2;
+    // zahl /= 2;
+    // printf("%i",bit);
+    return 0;
+}
