034f86fd162f6252c064b4a7315c4626b434ff66
bAsrOth <[basroth@protonmail.com]>
2023-06-06 22:51:34 +0200

    Franzi

---
+++ BMC_C/main_franzi.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <conio.h>
+  
+char getDNABase(void){
+
+    char eingabe; 
+    
+    while (1){
+        eingabe = getch(); 
+
+        if (eingabe == 'A' || eingabe == 'a'){
+            putchar ('A');
+            return 'A';
+        }
+        if (eingabe == 'C' || eingabe == 'c'){
+            putchar ('C');
+            return 'C';
+        }
+        if (eingabe == 'G' || eingabe == 'g'){
+            putchar ('G');
+            return 'G';
+        }
+        if (eingabe == 'T' || eingabe == 't'){
+            putchar ('T');
+            return 'T';
+        }
+        else if (eingabe == 10 || eingabe == 13){
+            return '\n';
+        }
+        else {
+            putchar (7); 
+        }
+
+    }
+    return eingabe; 
+
+}
+
+int main(void){
+    char c; 
+    c = getDNABase(); 
+
+    getch(); 
+    return 0; 
+}
