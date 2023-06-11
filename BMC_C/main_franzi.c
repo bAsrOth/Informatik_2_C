a6ce2ca4ac8ef138e45668e67a92e24ba30ec1bd
bAsrOth <[basroth@protonmail.com]>
2023-06-11 06:08:21 +0200

    Franzi

--- BMC_C/main_franzi.c
+++ BMC_C/main_franzi.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <conio.h>
-  
+
+
+char getDNABase(void);  
 char getDNABase(void){
 
     char eingabe; 
@@ -8,35 +10,46 @@ char getDNABase(void){
     while (1){
         eingabe = getch(); 
 
-        if (eingabe == 'A' || eingabe == 'a'){
-            return 'A';
-            putchar ('A');
+        if (eingabe == 'A'){
+            putchar (eingabe); 
         }
-        if (eingabe == 'C' || eingabe == 'c'){
-            putchar ('C');
-            return 'C';
+        else if (eingabe == 'a'){
+            putchar (65); 
         }
-        if (eingabe == 'G' || eingabe == 'g'){
-            putchar ('G');
-            return 'G';
+        else if (eingabe == 'C'){
+            putchar (eingabe); 
         }
-        if (eingabe == 'T' || eingabe == 't'){
-            putchar ('T');
-            return 'T';
+        else if (eingabe == 'c'){
+            putchar (67); 
         }
-        else if (eingabe == 10 || eingabe == 13){
-            return '\n';
+        else if (eingabe == 'G'){
+            putchar (eingabe); 
+        }
+         else if (eingabe == 'g'){
+            putchar (71); 
+        }
+        else if (eingabe == 'T'){
+            putchar (eingabe); 
+        }
+        else if (eingabe == 't'){
+            putchar (84); 
+        }
+        else if (eingabe == 10 ||13){
+            putchar (eingabe); 
         }
         else {
             putchar (7); 
         }
+
     }
+
     return eingabe; 
 
 }
 
 int main(void){
-
+    char c; 
+    c = getDNABase(); 
 
     getch(); 
     return 0; 
